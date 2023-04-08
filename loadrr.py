e8a021f83bdbdef2f60391fc8e3735570374b95a
digital-values <129333509+digital-values@users.noreply.github.com>
2023-04-07 23:54:29 -0400

    added code changes to .py

--- loadrr.py
+++ loadrr.py
@@ -2,13 +2,14 @@
 import simplekml
 import pandas as pd
 
-# Read in the CSV file of coordinates
+print("Make sure your file is titled 'coordinates.csv'")
+
 df = pd.read_csv('coordinates.csv')
 
-# Create a KML file to hold the placemarks
 kml = simplekml.Kml()
 
 #update kml file with user input of address long lat conversion
+print("Please input subject's top locations in the following format: 'Street Address, City, State, Zip code'")
 
 # Loop through each row in the CSV file and add a placemark to the KML file
 for index, row in df.iterrows():
@@ -16,4 +17,4 @@ for index, row in df.iterrows():
     kml.newpoint(name=f"Coordinate {index+1}", coords=[(row['Longitude'], row['Latitude'])])
 
 # Save the KML file
-kml.save('coordinates.kml')
+kml.save('mapped_coordinates.kml')
