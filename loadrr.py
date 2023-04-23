8ea31fdfb92a9874dc78c619bbf73fd463446132
digital-values <129333509+digital-values@users.noreply.github.com>
2023-04-23 06:27:59 -0400

    updated code on .py & actually added mock lat/longs

--- loadrr.py
+++ loadrr.py
@@ -11,17 +11,17 @@ df = pd.read_csv('coordinates.csv')
 print("Please input your subject's top three addresses below.\n")
 
 address1 = input("Address 1: ")
-address2 = input("\nAddress 2: ")
-address3 = input("\nAddress 3: ")
+#address2 = input("\nAddress 2: ")
+#address3 = input("\nAddress 3: ")
 
 # uses geopy to get the latitude and longitude coordinates for the addresses from user input
 geolocator = Nominatim(user_agent="loadrr")
 location1 = geolocator.geocode(address1)
-location2 = geolocator.geocode(address2)
-location3 = geolocator.geocode(address3)
+l#ocation2 = geolocator.geocode(address2)
+#location3 = geolocator.geocode(address3)
 new_coord1 = (location1.longitude, location1.latitude)
-new_coord2 = (location2.longitude, location2.latitude)
-new_coord3 = (location3.longitude, location3.latitude)
+#new_coord2 = (location2.longitude, location2.latitude)
+#new_coord3 = (location3.longitude, location3.latitude)
 
 # saves coordinates in a kml file 
 kml = simplekml.Kml()
@@ -34,10 +34,10 @@ for index, row in df.iterrows():
 # adds new place markers to the kml file
 new_point1 = kml.newpoint(name=f"{address1}", coords=[new_coord1])
 new_point1.style.iconstyle.color = simplekml.Color.red  
-new_point2 = kml.newpoint(name=f"{address2}", coords=[new_coord2])
-new_point2.style.iconstyle.color = simplekml.Color.green  
-new_point3 = kml.newpoint(name=f"{address3}", coords=[new_coord3])
-new_point3.style.iconstyle.color = simplekml.Color.purple  
+#new_point2 = kml.newpoint(name=f"{address2}", coords=[new_coord2])
+#new_point2.style.iconstyle.color = simplekml.Color.green  
+#new_point3 = kml.newpoint(name=f"{address3}", coords=[new_coord3])
+#new_point3.style.iconstyle.color = simplekml.Color.purple  
 
 print("\nAddress 1 will be plotted as a red pin. \nAddress 2 will be plotted as a green pin. \nAddress 3 will be plotted as a purple pin\n")
 
